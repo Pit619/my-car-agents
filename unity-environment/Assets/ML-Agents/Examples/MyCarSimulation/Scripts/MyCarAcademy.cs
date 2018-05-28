5416151c0528796e2710d16867329c65901945af
Sascha <sascha.luethje@tuhh.de>
2018-05-28 15:48:12 +0200

    First commit

---
+++ unity-environment/Assets/ML-Agents/Examples/MyCarSimulation/Scripts/MyCarAcademy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCarAcademy : Academy {
+
+	// Use this for initialization
+	public override void AcademyReset()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	public override void AcademyStep()
+	{
+		
+	}
+}
---
+++ unity-environment/Assets/ML-Agents/Examples/MyCarSimulation/Scripts/MyCarAgent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCarAgent : Agent {
+	
+	public GameObject car;
+
+	void Start()
+	{
+		
+	}
+
+	public override void AgentAction(float[] action, string textAction)
+	{
+		//Set rotation
+		Debug.Log(action[0]);
+		car.transform.Rotate(new Vector3(0, 0, 1), action[0]);
+	}
+
+	public override void CollectObservations()
+	{
+		List<float> state = new List<float>();
+
+	}
+
+	public override void AgentReset()
+	{
+		
+	}
+}
